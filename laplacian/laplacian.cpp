a3cf7c6fec97382fabb3f45fece6e18dbdbf8b75
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-01-18 22:39:04 -0300

    alocação da matriz dinamica 2D

--- laplacian/laplacian.cpp
+++ laplacian/laplacian.cpp
@@ -6,9 +6,17 @@ JacobiRelaxantion::JacobiRelaxantion(int rows, int cols, double tol, int max_int
     : rows(rows), cols(cols), tol(tol), max_interation(max_interation) {
         A = allocate_matrix(rows, cols);
         A_new = allocate_matrix(rows, cols);
-    }
+}
 
 JacobiRelaxantion::~JacobiRelaxantion(){
     deallocate_matrix(A, rows);
     deallocate_matrix(A_new, rows);
+}
+
+double** JacobiRelaxantion::allocate_matrix(int rown, int cols){
+    double** matrix = new double*[rows];
+    for (int i = 0; i < rows; ++i){
+        matrix[i] = new double[cols]();
+    }
+    return matrix;
 }
