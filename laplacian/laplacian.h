7512d831f97ba94b883a6600facacd96a6533be6
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-01-18 22:02:54 -0300

    criação da classe para o método da relaxação de Jacobi

--- laplacian/laplacian.h
+++ laplacian/laplacian.h
@@ -1,4 +1,15 @@
 #ifndef JACOBIRELAXATION_H
 #define JACOBIRELAXATION_H
 
+#include <string>
+
+class JacobiRelaxantion {
+private:
+    int rows, cols; // linhas e colunas para dimensão da malha
+    double tol;
+    int max_interation;
+    double** A;
+    double** A_new;
+};
+
 #endif // JACOBIRELAXATION_H
