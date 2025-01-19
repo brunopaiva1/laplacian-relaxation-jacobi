cfe9158d89d7284257492170eab01723baa052e0
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-01-18 22:28:19 -0300

    inplementação do construtor no .cpp

---
+++ laplacian/laplacian.cpp
@@ -0,0 +1,9 @@
+#include "laplacian.h"
+#include <iostream>
+#include <cmath>
+
+JacobiRelaxantion::JacobiRelaxantion(int rows, int cols, double tol, int max_interation)
+    : rows(rows), cols(cols), tol(tol), max_interation(max_interation) {
+        A = allocate_matrix(rows, cols);
+        A_new = allocate_matrix(rows, cols);
+    }
