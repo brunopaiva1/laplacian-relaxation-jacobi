663602ce0208714735ae6382ae81b8ce5ce20850
brunopaiva1 <brunopaivamaster10@gmail.com>
2025-01-19 00:16:23 -0300

    chamada das funções

--- laplacian/main.cpp
+++ laplacian/main.cpp
@@ -7,5 +7,11 @@ int main() {
     double tol = 1.0e-6;
     int max_interation = 1000;
 
+    JacobiRelaxation jacobi(rows, cols, tol, max_interation);
+
+    jacobi.initialize();
+    jacobi.solve();
+    jacobi.print_results();
+
     return 0;
 }
